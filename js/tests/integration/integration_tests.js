e10e65b7236532e41422e8e379caa19a95091c5d
toran billups <toranb@gmail.com>
2014-04-03 21:53:08 -0500

    added all the missing integration tests

--- js/tests/integration/integration_tests.js
+++ js/tests/integration/integration_tests.js
@@ -1,9 +1,11 @@
+import Person from 'js/models/person';
+
 var App;
 
 module('integration tests', {
     setup: function() {
         App = startApp();
-        //App.Person.people = [];
+        Person.people = [];
     },
     teardown: function() {
         $.mockjaxClear();
@@ -11,37 +13,37 @@ module('integration tests', {
     }
 });
 
-// test('empty ajax response will yield empty table', function() {
-//     stubEndpointForHttpRequest('/api/people', []);
-//     visit("/").then(function() {
-//         missing("table tr");
-//     });
-// });
+test('empty ajax response will yield empty table', function() {
+    stubEndpointForHttpRequest('/api/people', []);
+    visit("/").then(function() {
+        missing("table tr");
+    });
+});
 
-// test('ajax response with 2 people yields table with 2 rows', function() {
-//     var json = [{firstName: "x", lastName: "y"}, {firstName: "h", lastName: "z"}];
-//     stubEndpointForHttpRequest('/api/people', json);
-//     visit("/").then(function() {
-//         var rows = find("table tr").length;
-//         equal(rows, 2, rows);
-//     });
-// });
+test('ajax response with 2 people yields table with 2 rows', function() {
+    var json = [{firstName: "x", lastName: "y"}, {firstName: "h", lastName: "z"}];
+    stubEndpointForHttpRequest('/api/people', json);
+    visit("/").then(function() {
+        var rows = find("table tr").length;
+        equal(rows, 2, rows);
+    });
+});
 
-// test('another empty ajax response will yield another empty table', function() {
-//     stubEndpointForHttpRequest('/api/people', []);
-//     visit("/").then(function() {
-//         missing("table tr");
-//     });
-// });
+test('another empty ajax response will yield another empty table', function() {
+    stubEndpointForHttpRequest('/api/people', []);
+    visit("/").then(function() {
+        missing("table tr");
+    });
+});
 
-// otest('ajax response with 1 person yields table with 1 row', function() {
-//     var matt = {firstName: 'matt', lastName: 'morrison'};
-//     stubEndpointForHttpRequest('/api/people', [matt]);
-//     visit("/").then(function() {
-//         var rows = find("table tr").length;
-//         equal(rows, 1, rows);
-//     });
-// });
+test('ajax response with 1 person yields table with 1 row', function() {
+    var matt = {firstName: 'matt', lastName: 'morrison'};
+    stubEndpointForHttpRequest('/api/people', [matt]);
+    visit("/").then(function() {
+        var rows = find("table tr").length;
+        equal(rows, 1, rows);
+    });
+});
 
 test('add will append another person to the html table', function() {
     expect(4);
@@ -61,19 +63,19 @@ test('add will append another person to the html table', function() {
   });
 });
 
-// test('delete will remove the person for a given row', function() {
-//     expect(5);
-//     var matt = {firstName: 'matt', lastName: 'morrison'};
-//     var toran = {firstName: 'toran', lastName: 'billups'};
-//     stubEndpointForHttpRequest('/api/people', [matt, toran]);
-//     visit("/").then(function() {
-//         var rows = find("table tr").length
-//         equal(rows, 2, "the table had " + rows + " rows");
-//         equal(find("table tr:eq(0) td:eq(0)").text(), "matt morrison", "the first row was incorrect");
-//         equal(find("table tr:eq(1) td:eq(0)").text(), "toran billups", "the first row was incorrect");
-//         return click("table .delete:first");
-//     }).then(function() {
-//         equal(find("table tr").length, 1, "the table of people was not complete");
-//         equal(find("table tr:eq(0) td:eq(0)").text(), "toran billups", "the wrong person was deleted");
-//     });
-// });
+test('delete will remove the person for a given row', function() {
+    expect(5);
+    var matt = {firstName: 'matt', lastName: 'morrison'};
+    var toran = {firstName: 'toran', lastName: 'billups'};
+    stubEndpointForHttpRequest('/api/people', [matt, toran]);
+    visit("/").then(function() {
+        var rows = find("table tr").length
+        equal(rows, 2, "the table had " + rows + " rows");
+        equal(find("table tr:eq(0) td:eq(0)").text(), "matt morrison", "the first row was incorrect");
+        equal(find("table tr:eq(1) td:eq(0)").text(), "toran billups", "the first row was incorrect");
+        return click("table .delete:first");
+    }).then(function() {
+        equal(find("table tr").length, 1, "the table of people was not complete");
+        equal(find("table tr:eq(0) td:eq(0)").text(), "toran billups", "the wrong person was deleted");
+    });
+});
